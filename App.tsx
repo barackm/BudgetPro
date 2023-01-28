902e233aca0eaddacd691125d1279897b3c78597
Barack Mukelenga <barackmukelenga100@gmail.com>
2023-01-28 14:59:08 +0200

    Add support for icons

--- App.tsx
+++ App.tsx
@@ -2,6 +2,9 @@ import React from 'react';
 import { Screen } from './src/components/Screen/Screen';
 import Button from './src/components/common/Button';
 import { createTheme, ThemeProvider } from './src/theme/ThemeProvider';
+import Ionicons from 'react-native-vector-icons/Ionicons';
+import { colors } from './src/theme';
+import { getIconSize } from './src/utlis/icons';
 
 const App = () => {
   const theme = createTheme({});
@@ -10,9 +13,15 @@ const App = () => {
     <ThemeProvider theme={theme}>
       <Screen>
         <Button
+          startIcon={
+            <Ionicons
+              name="ios-add"
+              size={getIconSize()}
+              color={colors.white}
+            />
+          }
           variant="contained"
-          onPress={() => console.log('Pressed')}
-          loading>
+          onPress={() => console.log('Pressed')}>
           Content
         </Button>
       </Screen>
